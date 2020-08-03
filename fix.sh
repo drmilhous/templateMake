35aa6d9b6502514a0e83cdf565e000243e0c79b3
Matt <milhous.30@gmail.com>
2020-08-03 10:44:15 -0500

    adding fix

--- fix.sh
+++ fix.sh
@@ -4,7 +4,7 @@ fi
 cp $HOME/templateMake/genMake.sh $HOME/bin
 cp $HOME/templateMake/genMake64.sh $HOME/bin
 cp $HOME/templateMake/genC.sh $HOME/bin
-echo "PATH=$PATH:$HOME/bin/" >> $HOME/.bashrc
+echo "PATH=$PATH:$HOME/bin/:$HOME/templateMake/bin" >> $HOME/.bashrc
 if [ ! -f $HOME/projects ]; then
 	mkdir $HOME/projects
 fi
